4715ed0fd9affd22c5fd6301678074cd6b0df201
kamonotakahashi <kamonotakahashi@gmail.com>
2020-04-07 12:34:31 +0900

    APIのsyuusei

--- modules/commandAction.js
+++ modules/commandAction.js
@@ -9,14 +9,20 @@ module.exports = {
             if(!action) return null;
             return this[action[1]](action[2].split(' '), callback);
         } catch (e) {
+            console.log(e)
             return null;
         }
     },
 
     history(options, callback) {
+        let player = typeof options[0] != 'undefined' ? options[0].toLowerCase() : '';
+        let limit = typeof options[1] != 'undefined' ? Number(options[1]) : 10;
 
-        let player = options[0];
-        let limit = options[1] ? options[1] : 10;
+        //バリデーション入れるまで　バグ回避用にへんてこコードを追加しとくぜ！
+        if( !Number.isInteger(limit) || limit < 1 || player.length < 2) {
+            callback.send("フォーマットが正しくありません\n例：　/history <プレイヤー名> <自然数>　");
+            return;
+        }
 
         axios.get(
             process.env.BASE_API_URL + 'recent_win_ratio_bulk',
@@ -30,12 +36,15 @@ module.exports = {
             let result = '';
             if(res.data.err == '') {
                 result = res.data.results.find( res => {
-                    return player.toLowerCase() == res.name.toLowerCase();
+                    return player == res.name.toLowerCase();
                 });
                 callback.send(
                     result ? `${result.name} さんの直近 ${limit} 戦分のデータ\n勝利：${result.win}　敗北：${result.lost}　勝率：${result.win_ratio}%` : `${player}さんって人はいないです・・・`
                 );
             }
+        })
+        .catch ( error => {
+            callback.send("おや、BOTの様子がおかしいようだ。");
         });
 
     }
