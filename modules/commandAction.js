f522ba65d47bdc7c2549bc2de2eba126d0dcd344
kamonotakahashi <kamonotakahashi@gmail.com>
2020-04-08 17:36:23 +0900

    ヘルプコマンド、ライブコマンドの実装

--- modules/commandAction.js
+++ modules/commandAction.js
@@ -1,5 +1,6 @@
 const httpClient = require("./httpClient.js");
 const notifiyAlertClient = require("./httpClient.js");
+const commandConfig = require("../config/commandConfig.json");
 const path = require("path");
 const agh = require('agh.sprintf');
 require('dotenv').config();
@@ -11,11 +12,17 @@ notifiyAlertClient.initialize(
 
 module.exports = {
     parser(command, callback, botClient) {
-        let action = command.match(/^\/([a-zA-Z]+)\s([a-zA-Z0-9\s_-]+)+?/);
+        let action;
+        if(command.indexOf(' ') !== -1) {
+            action = command.match(/^\/([a-zA-Z]+)\s?([a-zA-Z0-9\s_-]+)+?/);
+        } else {
+            action = command.match(/^\/([a-zA-Z^\s]+)/);
+        }
 
         try {
             if(!action) return null;
-            this[action[1]](action[2].split(' '), callback, botClient)
+            if(action.length == 3) this[action[1]](callback, botClient, action[2].split(' '));
+            if(action.length == 2) this[action[1]](callback, botClient);
         } catch (e) {
             throw e;
         }
@@ -24,7 +31,8 @@ module.exports = {
     /*
      * 試合数から勝率データを表示する
      */
-    history(options, callback, botClient) {
+    history(callback, botClient, options = []) {
+
         let player = typeof options[0] != 'undefined' ? options[0].toLowerCase() : '';
         let limit = typeof options[1] != 'undefined' ? Number(options[1]) : 10;
 
@@ -54,7 +62,6 @@ module.exports = {
         })
         .catch ( err => {
             notifiyAlertClient.post({content: err});
-            callback.channel.send(err);
         });
 
         return;
@@ -63,7 +70,7 @@ module.exports = {
     /*
      * タウントを鳴らす
      */
-    taunt(options, callback, botClient) {
+    taunt(callback, botClient, options = []) {
         let tauntNo = typeof options[0] != 'undefined' ? Number(options[0]) : null;
 
         //バリデーション入れるまで　バグ回避用にへんてこコードを追加しとくぜ！
@@ -87,9 +94,33 @@ module.exports = {
 
         })
         .catch( err => {
-            console.log(err);
+            notifiyAlertClient.post({content: err});
         });
 
         return;
+    },
+
+    /*
+     * 生放送リスト
+     */
+    live(callback, botClient) {
+        callback.channel.send("準備中・・・");
+        return;
+    },
+
+    /*
+     * ヘルプ
+     */
+    help(callback, botClient) {
+        let commandList = ['```', '', '```'];
+
+        Object.keys(commandConfig).forEach( key => {
+            commandList[1] += `${commandConfig[key].command}\n\t\t${commandConfig[key].description}\n\n`;
+        });
+
+        callback.channel.send(commandList.join(''));
+
+        return;
     }
+
 }
